f548c8c3d30b9ff7c0b001364167b19cecdbf772
YanaY <yaroshevich.yana@gmail.com>
2015-03-16 18:57:03 +0300

    localStorage: saving data

--- GUI/scripts.js
+++ GUI/scripts.js
@@ -1,47 +1,53 @@
 'use strict'
 
-var name = "";
 var editFlag = false;
-var editMsgPos = -1;
+var id = -1;
+
+var uniqueId = function() {
+	var date = Date.now();
+	var random = Math.random() * Math.random();
+	return Math.floor(date * random).toString();
+};
+
+var theMessage = function(msgText, userName) {
+	return {
+		text: msgText,
+		name: userName,
+		id: uniqueId()
+	};
+};
+
+var page = null;
 
 function run(){
-	serverCheck(true);
 	document.addEventListener('click', delegateEvent);
+	page = restore() || {	
+							messages: [theMessage('Welcome!', 'Admin')],
+							name: "",
+							cond: true
+						};	
+	createPage(page);
 }
 
 function delegateEvent(evtObj) {
 	if(evtObj.type === 'click'){
-		if (evtObj.target.classList.contains('btn-success') || evtObj.target.classList.contains('btn-info'))
+		if (evtObj.target.classList.contains('btn-success') 
+			|| evtObj.target.classList.contains('btn-info'))
 			onInputNameButtonClick(evtObj);
 		else if (evtObj.target.classList.contains('btn-primary')){
 			onSendMsgButtonClick(evtObj);
 		}
-		else if (evtObj.target.classList.contains('btn-default') || evtObj.target.classList.contains('glyphicon')){
+		else if (evtObj.target.classList.contains('btn-default') 
+			|| evtObj.target.classList.contains('glyphicon')){
 			onEditMsgButtonClick(evtObj);
 		}
 	}
 }
 
-function onSendMsgButtonClick(evtObj){
-	var textField = document.getElementById('inputMsgText');
-	(editFlag) ? sendEditedMsg(textField.value, evtObj) : sendMsg(textField.value, evtObj);
-	textField.value = '';
-}
-
-function onInputNameButtonClick(evtObj){
-	var nameField;
-	nameField = (evtObj.target.classList.contains('btn-success')) ? 
-				document.getElementById('nameInputText') : document.getElementById('nameChangeText'); 
-	setName(nameField.value, evtObj);
-	nameField.value = '';
-}
-
-function onEditMsgButtonClick(evtObj){
-	var items = document.getElementsByClassName('history')[0]; 
-	var msg = (evtObj.target.hasChildNodes()) ? evtObj.target.parentElement : evtObj.target.parentElement.parentElement;
-	var children = msg.childNodes;
-	var indicator = (evtObj.target.hasChildNodes()) ? evtObj.target.firstElementChild.className : evtObj.target.className;
-	(indicator == "glyphicon glyphicon-wrench") ? editMsg(evtObj, children, msg) : items.removeChild(msg);
+function greetingCreation(value){
+	var greeting = (document.getElementsByTagName('h3')[0]) || document.createElement('h3');
+	greeting.innerHTML = 'Hello, ' + page.name + '!';
+	return greeting; 
 }
 
 function setAttr(obj, attrType, attrValue){
@@ -50,10 +56,10 @@ function setAttr(obj, attrType, attrValue){
 	obj.setAttributeNode(attr);
 }
 
-function greetingCreation(value){
-	var greeting = document.createElement('h3');
-	greeting.innerHTML = 'Hello, ' + name + '!';
-	return greeting; 
+function childCreation(value, elType){
+	var child = document.createElement(elType);
+	setAttr(child, 'class', value);
+	return child;
 }
 
 function btnCreation(btnClass){
@@ -69,14 +75,9 @@ function btnCreation(btnClass){
 	return btn;
 }
 
-function childCreation(value, elType){
-	var child = document.createElement(elType);
-	setAttr(child, 'class', value);
-	return child;
-}
-
-function createMsg(value){
+function createMsg(name, value, id){
 	var userMessage = childCreation("userMessage", 'div');
+	setAttr(userMessage, 'id', id);
 
 	var userName = childCreation("userName", 'div');
 	userName.innerHTML = name;
@@ -95,67 +96,136 @@ function createMsg(value){
 	return userMessage;
 }
 
-function setName(value, evtObj){
-	if(!value){
-		return;
-	}
-
-	var prevName = name;
-	name = value;
+function updateName(page){
 	var items = document.getElementsByClassName('inputName')[0];
-	var greeting = greetingCreation(name);
-
-	if(evtObj.target.classList.contains('btn-info')){
-		var h3 = document.getElementsByTagName('h3')[0];
-		h3.innerHTML = 'Hello, ' + name + '!';
-	}
-
-	else{
-		items.appendChild(greeting);
-		document.getElementById("sendBtn").style.visibility = "visible";
-		document.getElementById("coolMan").style.visibility = "visible";
-		document.getElementById("inputMsgText").style.visibility = "visible";
-	}
-
+	var greeting = greetingCreation(page.name);
+	items.appendChild(greeting);
+	document.getElementById("sendBtn").style.visibility = "visible";
+	document.getElementById("coolMan").style.visibility = "visible";
+	document.getElementById("inputMsgText").style.visibility = "visible";
 	document.getElementById("form1").style.visibility = "hidden";
 	document.getElementById("form2").style.visibility = "visible";
 }
 
-function editMsg(evtObj, children, msg){
-	var textToChange = '';
-	for (var i = 0; i < children.length; i++){
-		if(children[i].className == "text"){
-			textToChange = children[i].innerHTML;
+function createPage(page){
+	serverCheck(page.cond);
+	if(page.name.length > 0){
+		updateName(page);		
+	}
+
+	var items = document.getElementsByClassName('history')[0];
+	while(items.childNodes[0]){
+		items.removeChild(items.childNodes[0]);
+	}	
+	for(var i = 0; i < page.messages.length; i++){
+		var msg = page.messages[i];
+		var userMessage = createMsg(msg.name, msg.text, msg.id);
+		items.appendChild(userMessage);
+	}
+}
+
+function onInputNameButtonClick(evtObj){
+	var nameField;
+	nameField = (evtObj.target.classList.contains('btn-success')) ? 
+				document.getElementById('nameInputText') : document.getElementById('nameChangeText');
+	setName(nameField.value);
+	nameField.value = '';
+	createPage(page);
+	store(page);
+}
+
+function onSendMsgButtonClick(evtObj){
+	var textField = document.getElementById('inputMsgText');
+	(editFlag) ? sendEditedMsg(textField.value, evtObj) : sendMsg(textField.value, evtObj);
+	textField.value = '';
+	createPage(page);
+	store(page);
+}
+
+function onEditMsgButtonClick(evtObj){
+	var msg = (evtObj.target.hasChildNodes()) ? evtObj.target.parentElement : evtObj.target.parentElement.parentElement;
+	id = msg.getAttribute('id');
+	var indicator = (evtObj.target.hasChildNodes()) ? evtObj.target.firstElementChild.className : evtObj.target.className;
+	(indicator == "glyphicon glyphicon-wrench") ? editMsg(id) : removeMsg(id);
+}
+
+function editMsg(id){
+	var textToChange = "";
+	for (var i = 0; i < page.messages.length; i++){
+		var msg = page.messages[i];
+		if(msg.id == id && msg.name == page.name){
+			textToChange = msg.text;
 			break;
+		}
+		else if (msg.id == id){
+			alert("you can't change this!");
 		}
 	}
+	
 	var field = document.getElementById("inputMsgText");
 	field.value = textToChange;
+	editFlag = true;	
+}
 
-	editFlag = true;
-	var items = document.getElementsByClassName('history')[0];
-	for (var i = 0; i < items.childNodes.length; i++){
-		if (items.childNodes[i] == msg){
-			editMsgPos = i;
+function removeMsg(id){
+	for (var i = 0; i < page.messages.length; i++){
+		var msg = page.messages[i];
+		if(msg.id == id && msg.name == page.name){
+			for (var j = i; j < page.messages.length - 1; j++)
+				page.messages[j] = page.messages[j + 1];
+			page.messages.pop();
 			break;
 		}
-	}	
+		else if (msg.id == id){
+			alert("you can't delete this!");
+		}
+	}
+	createPage(page);
+	store(page);
 }
 
 function sendMsg(value, evtObj){
 	if(!value){
 		return;
 	}
-	var items = document.getElementsByClassName('history')[0];
-	var userMessage = createMsg(value);
-	items.appendChild(userMessage);
+	var objMsg = theMessage(value, page.name);
+	page.messages.push(objMsg);
 }
 
 function sendEditedMsg(value, evtObj){
-	var items = document.getElementsByClassName('history')[0];
-	var userMessage = createMsg(value);
-	items.replaceChild(userMessage, items.childNodes[editMsgPos]);
+	for (var i = 0; i < page.messages.length; i++){
+		var msg = page.messages[i];
+		if(msg.id == id){
+			page.messages[i] = theMessage(value, page.name);
+			break;
+		}
+	}
 	editFlag = false;
+}
+
+function setName(value){
+	if(!value){
+		return;
+	}
+	page.name = value;
+}
+
+function store(pageToSave) {
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
+	localStorage.clear();
+	localStorage.setItem("Chatting page", JSON.stringify(pageToSave));
+}
+
+function restore() {
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
+	var item = localStorage.getItem("Chatting page");
+	return item && JSON.parse(item);
 }
 
 function serverCheck(flag){
