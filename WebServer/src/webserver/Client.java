3a3b44c794e3a6688882f81c28d9f76ecd36ad58
YanaY <yaroshevich.yana@gmail.com>
2015-03-22 21:24:28 +0300

    logging

---
+++ WebServer/src/webserver/Client.java
@@ -0,0 +1,145 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Date;
+
+public class Client implements Runnable {
+    private List<String> history = new ArrayList<String>();
+    private MessageExchange messageExchange = new MessageExchange();
+    private String host;
+    private Integer port;
+    private PrintWriter out;
+    private Date d;
+
+    public Client(String host, Integer port) throws IOException{
+        this.host = host;
+        this.port = port;
+        out = new PrintWriter(new BufferedWriter(new FileWriter(new File("clientlog.txt"))));
+    }
+
+    public static void main(String[] args) throws IOException{
+        if (args.length != 2)
+            System.out.println("Usage: java ChatClient host port");
+        else {
+            System.out.println("Connection to server...");
+            String serverHost = args[0];
+            Integer serverPort = Integer.parseInt(args[1]);
+            Client client = new Client(serverHost, serverPort);
+            new Thread(client).start();
+            System.out.println("Connected to server: " + serverHost + ":" + serverPort);
+            client.listen();
+        }
+    }
+
+    private HttpURLConnection getHttpURLConnection() throws IOException {
+        URL url = new URL("http://" + host + ":" + port + "/chat?token=" + messageExchange.getToken(history.size()));
+        d = new Date();
+        out.println(d.toLocaleString() + " request parameters: url: " + url);
+        out.flush();
+        return (HttpURLConnection) url.openConnection();
+    }
+
+    public List<String> getMessages() {
+        List<String> list = new ArrayList<String>();
+        HttpURLConnection connection = null;
+        try {
+            d = new Date();
+            out.println(d.toLocaleString() + " request begin");
+            out.println(d.toLocaleString() + " request method: GET");
+            out.flush();
+            connection = getHttpURLConnection();
+            connection.connect();
+            String response = messageExchange.inputStreamToString(connection.getInputStream());
+            JSONObject jsonObject = messageExchange.getJSONObject(response);
+            JSONArray jsonArray = (JSONArray) jsonObject.get("messages");
+            d = new Date();
+            out.println(d.toLocaleString() + " server response parameters: messages: " + jsonArray + " token: " + jsonObject.get("token"));
+            out.flush();
+            for (Object o : jsonArray) {
+                System.out.println(o);
+                list.add(o.toString());
+            }
+        } catch (IOException e) {
+            System.err.println("ERROR: " + e.getMessage());
+        } catch (ParseException e) {
+            System.err.println("ERROR: " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+                d = new Date();
+                out.println(d.toLocaleString() + " request end");
+                out.flush();
+            }
+        }
+
+        return list;
+    }
+
+    public void sendMessage(String message) {
+        HttpURLConnection connection = null;
+        try {
+            d = new Date();
+            out.println(d.toLocaleString() + " request begin");
+            out.println(d.toLocaleString() + " request method: POST");
+            out.flush();
+            connection = getHttpURLConnection();
+            connection.setDoOutput(true);
+
+            connection.setRequestMethod("POST");
+
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+
+            byte[] bytes = messageExchange.getClientSendMessageRequest(message).getBytes();
+            wr.write(bytes, 0, bytes.length);
+            wr.flush();
+            wr.close();
+            d = new Date();
+            out.println(d.toLocaleString() + " request parameters: message: " + message);
+            out.flush();
+            connection.getInputStream();
+
+        } catch (IOException e) {
+            System.err.println("ERROR: " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+                d = new Date();
+                out.println(d.toLocaleString() + " request end");
+                out.flush();
+            }
+        }
+    }
+
+    public void listen() {
+        while (true) {
+            List<String> list = getMessages();
+            if (list.size() > 0) {
+                history.addAll(list);
+            }
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                System.err.println("ERROR: " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String message = scanner.nextLine();
+            sendMessage(message);
+        }
+    }
+}
---
+++ WebServer/src/webserver/Server.java
@@ -0,0 +1,153 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.json.simple.parser.ParseException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.text.SimpleDateFormat;
+
+import java.io.*;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Date;
+
+public class Server implements HttpHandler {
+    private List<String> history = new ArrayList<String>();
+    private MessageExchange messageExchange = new MessageExchange();
+    private JSONParser jsonParser = new JSONParser();
+    private PrintWriter out;
+    private Date d;
+
+    public Server() throws IOException{
+         out = new PrintWriter(new BufferedWriter(new FileWriter(new File("serverlog.txt"))));
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1)
+            System.out.println("Usage: java Server port");
+        else {
+            try {
+                System.out.println("Server is starting...");
+                Integer port = Integer.parseInt(args[0]);
+                HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+                System.out.println("Server started.");
+                String serverHost = InetAddress.getLocalHost().getHostAddress();
+                System.out.println("Get list of messages: GET http://" + serverHost + ":" + port + "/chat?token={token}");
+                System.out.println("Send message: POST http://" + serverHost + ":" + port + "/chat provide body json in format {\"message\" : \"{message}\"} ");
+
+                server.createContext("/chat", new Server());
+                server.setExecutor(null);
+                server.start();
+            } catch (IOException e) {
+                System.out.println("Error creating http server: " + e);
+            }
+        }
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        d = new Date();
+        out.println(d.toLocaleString() + " request begin");
+        out.flush();
+        String response = "";
+        if ("GET".equals(httpExchange.getRequestMethod())) {
+            response = doGet(httpExchange);
+            
+        } else if ("POST".equals(httpExchange.getRequestMethod())) {
+            doPost(httpExchange);
+        } else {
+            response = "Unsupported http method: " + httpExchange.getRequestMethod();
+            d = new Date();
+            out.println(d.toLocaleString() + " " + response);
+            out.flush();
+        }
+
+        sendResponse(httpExchange, response);
+        d = new Date();
+        out.println(d.toLocaleString() + " request end");
+        out.flush();
+    }
+
+    private String doGet(HttpExchange httpExchange) {
+        String query = httpExchange.getRequestURI().getQuery();
+        if (query != null) {
+            Map<String, String> map = queryToMap(query);
+            String token = map.get("token");
+            if (token != null && !"".equals(token)) {
+                d = new Date();
+                out.println(d.toLocaleString() + " request method: GET");
+                out.println(d.toLocaleString() + " request parameters: token: " + token);
+                out.flush();
+                int index = messageExchange.getIndex(token);
+                return messageExchange.getServerResponse(history.subList(index, history.size()));
+            } else {
+                return "Token query parameter is absent in url: " + query;
+                d = new Date();
+                out.println(d.toLocaleString() + " Token query parameter is absent in url: " + query);
+                out.flush();
+            }
+        }
+        d = new Date();
+        out.println(d.toLocaleString() + " Absent query in url");
+        out.flush();
+        return  "Absent query in url";
+    }
+
+    private void doPost(HttpExchange httpExchange) {
+        try {
+            String message = messageExchange.getClientMessage(httpExchange.getRequestBody());
+            d = new Date();
+            out.println(d.toLocaleString() + " request method: POST");
+            out.println(d.toLocaleString() + " request parameters: message: " + message);
+            out.flush();
+            System.out.println("Get Message from User : " + message);
+            history.add(message);
+        } catch (ParseException e) {
+            d = new Date();
+            out.println(d.toLocaleString() + " Invalid user message");
+            out.flush();
+            System.err.println("Invalid user message: " + httpExchange.getRequestBody() + " " + e.getMessage());
+        }
+    }
+
+    private void sendResponse(HttpExchange httpExchange, String response){
+        try {
+            d = new Date();
+            try{
+                JSONObject resp = (JSONObject) jsonParser.parse(response.trim());
+                out.println(d.toLocaleString() + " server response");
+                out.println(d.toLocaleString() + "response parameters: token: " + resp.get("token") + " messages: " + resp.get("messages"));
+                out.flush();
+            }
+            catch (ParseException p){}
+            byte[] bytes = response.getBytes();
+            httpExchange.sendResponseHeaders(200, bytes.length);
+
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(bytes);
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Map<String, String> queryToMap(String query) {
+        Map<String, String> result = new HashMap<String, String>();
+        for (String param : query.split("&")) {
+            String pair[] = param.split("=");
+            if (pair.length > 1) {
+                result.put(pair[0], pair[1]);
+            } else {
+                result.put(pair[0], "");
+            }
+        }
+        return result;
+    }
+}
